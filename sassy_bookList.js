675225d5b3177802a5c74160a60e4f5db4cb0763
Hackbright Student <user@mini-007.(none)>
2013-10-17 11:29:32 -0700

    Renamed file and sassy booklist complete per instructions

--- sassy_bookList.js
+++ sassy_bookList.js
@@ -9,10 +9,11 @@ var BookList = function(booksRead, booksUnread, nextBook, currentBook, lastBook)
         this.bookShelf.push(book);
     };
     this.finishCurrentBook = function() {
-        this.currentBook.readDate(Date.now());
+        this.currentBook.read = true;
+        this.currentBook.readDate = new Date(Date.now());
         this.lastBook = this.currentBook;
         this.currentBook = this.nextBook;
-        this.nextBook = this.bookShelf[book.readTrueofFalse == False];
+        this.nextBook = this.bookShelf[this.readTrueOrFalse === false];
     };
 };
 
@@ -29,7 +30,18 @@ var kamaSutra = new Book("Kama Sutra", "Erotica", "Anonymous", false);
 var theBible = new Book("The Bible", "Fiction", "God", true, new Date(2010, 9, 15));
 var micasBookList = new BookList(5, 3, kamaSutra, harryPotter, theBible);
 
-console.log(kamaSutra);
-console.log(theBible);
-console.log(harryPotter);
-console.log(micasBookList);
+// console.log(kamaSutra);
+// console.log(theBible);
+// console.log(harryPotter);
+// console.log(micasBookList);
+
+
+// micasBookList.finishCurrentBook();
+console.log("Empty bookshelf ", micasBookList.bookShelf);
+micasBookList.add(kamaSutra);
+micasBookList.add(harryPotter);
+micasBookList.add(theBible);
+console.log("With 3 added books: ", micasBookList.bookShelf);
+micasBookList.finishCurrentBook();
+console.log("After finishing HP: ", micasBookList.bookShelf);
+console.log("The new book I'm reading: ", micasBookList.currentBook);
