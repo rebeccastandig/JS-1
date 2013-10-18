ece179166aa79e46b954ed1ac1343c1a430f05d5
Hackbright Student <user@mini-007.(none)>
2013-10-17 17:37:54 -0700

    Started on read and unread book counts

--- sassy_bookList.js
+++ sassy_bookList.js
@@ -1,5 +1,7 @@
 var BookList = function(books){
     this.bookShelf = books;
+    this.numReadBooks = 0;
+    this.numUnreadBooks = 0;
 
     if (this.bookShelf) {
         for (var i = this.bookShelf.length - 1; i >= 0; i--) {
@@ -26,28 +28,34 @@ var BookList = function(books){
         };
     
 
-//     numReadBooks = this.readBooks(this.bookShelf);
-//     this.readBooks = function(){
-//         for (var i = this.bookShelf.length - 1; i >= 0; i--) {
-//             if (this.bookShelf[i].read) {
-//                 numReadBooks += 1;
-//                 return numReadBooks;
-//             }
-//         }
-//     };
+
+    for (var i = 0; i < this.bookShelf.length; i++) {
+        if (this.bookShelf[i].read) {
+            this.numReadBooks += 1;
+        }
+    };
+
+
+
+    for (var i = 0; i < this.bookShelf.length; i++) {
+        if (!this.bookShelf[i].read) {
+            this.numUnreadBooks += 1;
+        }
+    };
+
 
     this.finishCurrentBook = function() {
         if (this.currentBook) {
             this.currentBook.read = true;
+            this.numReadBooks += 1;
+            this.numUnreadBooks -= 1;
             this.currentBook.readDate = new Date(Date.now());
             this.lastBook = this.currentBook;
             
-
             for (var i = this.bookShelf.length - 1; i >= 0; i--) {
                 if (!this.bookShelf[i].read){
                     this.nextBook = this.bookShelf[i];
                     var reassigned = true;
-                    console.log("next book: ",this.nextBook)
                 }
             }
             if (!reassigned){
@@ -55,12 +63,10 @@ var BookList = function(books){
             }
             this.currentBook = this.nextBook;
         } else {
-                console.log("You don't have a current book. Go to the library!");
-
-            //update # of read books
-           
+                console.log("You don't have a current book. Go to the library!");           
         }
     };
+
 };
 
 books = [];
@@ -78,15 +84,16 @@ var harryPotter = new Book("Harry Potter", "Fiction", "J.K. Rowling", false);
 var kamaSutra = new Book("Kama Sutra", "Erotica", "Anonymous", false);
 var theBible = new Book("The Bible", "Fiction", "God", true, "15 Sep 2013");
 var greysAnatomy = new Book("Grey's Anatomy", "Medical", "Grey", true, "10 Sep 2004");
+var warAndPeace = new Book("War and Peace", "Fiction", "Leo Tolstoy", false);
 var littleWomen = new Book("Little Women", "Fiction", "Louisa May Alcott", true, "10 Sep 1999");
 var ourBookList = new BookList(books);
 
-// console.log(kamaSutra);
-// console.log(theBible);
-// console.log(harryPotter);
-console.log("current book: ", ourBookList.currentBook);
+
+// console.log("current book: ", ourBookList.currentBook);
 ourBookList.finishCurrentBook();
-console.log("current book now: ", ourBookList.currentBook);
-ourBookList.finishCurrentBook();
-console.log("current book now: ", ourBookList.currentBook);
-ourBookList.finishCurrentBook();
+console.log("Now I've read ", ourBookList.numReadBooks, " books.")
+console.log("And I have ", ourBookList.numUnreadBooks, " books left to read.")
+// console.log("current book now: ", ourBookList.currentBook);
+// ourBookList.finishCurrentBook();
+// console.log("current book now: ", ourBookList.currentBook);
+// ourBookList.finishCurrentBook();
